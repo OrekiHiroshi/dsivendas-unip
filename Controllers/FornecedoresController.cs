2fd689384ca9b68882fd9e8379e95303168b76f3
OrekiHiroshi <erikruas67@gmail.com>
2024-09-29 23:50:09 -0300

    Add files via upload

---
+++ Controllers/FornecedoresController.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+using DsiVendas.Models;
+
+namespace DsiVendas.Controllers;
+
+public class FornecedoresController(ApplicationDbContext context) : Controller
+{
+  public IActionResult Index()
+  {
+    var listaFornedores = context.Fornecedores.ToList();
+    return View(listaFornedores);
+  }
+  public List<Fornecedor> Api()
+  {
+    var listaFornecedor = context.Fornecedores.ToList();
+    return listaFornecedor;
+  }
+
+  public IActionResult Criar()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public IActionResult Criar(Fornecedor fornecedor)
+  {
+    context.Fornecedores.Add(fornecedor);
+    context.SaveChanges();  
+    
+    return RedirectToAction("Index");
+  }
+
+  public IActionResult Editar(int id)
+  {
+    var fornecedor = context.Fornecedores.Find(id);
+    if (fornecedor == null)
+    {
+      return NotFound();
+    }
+    return View(fornecedor);
+  }
+
+  [HttpPost]
+  public IActionResult Editar(Fornecedor fornecedor)
+  {
+      var fornecedorExistente = context.Fornecedores.Find(fornecedor.Id); 
+      if (fornecedorExistente == null)
+      {
+        return NotFound();
+      }
+      fornecedorExistente.Nome = fornecedor.Nome;
+      fornecedorExistente.Cidade = fornecedor.Cidade;
+      fornecedorExistente.Telefone = fornecedor.Telefone;
+      context.Fornecedores.Update(fornecedorExistente); 
+      context.SaveChanges(); 
+      return RedirectToAction("Index");
+
+  }
+}
